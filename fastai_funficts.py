928f60dbfe91852404a9ab3b572e9a062af3d6fa
AthanCB <athancb@hotmail.com>
2019-03-15 01:26:37 +0200

    unknown dsets added, fixed fastai predicts

--- fastai_funficts.py
+++ fastai_funficts.py
@@ -1,21 +1,57 @@
 from fastai.text import *
+from fastai.core import *
+from fastai import *
 import os
 
-path = "answers" + os.sep
+#from google.colab import drive
+#drive.mount('/content/drive')
+
+#path = "/content/drive/My Drive/Colab Notebooks/"
+path = 'answers' + os.sep
 df = pd.read_csv(path + 'ds0.csv')
+#df.count
+#df1 = pd.read_csv(path + 'ds1.csv')
+#df2 = pd.read_csv(path + 'ds2.csv')
+#df3 = pd.read_csv(path + 'ds3.csv')
+#df.append(df1)
+#df.append(df2)
+#df.append(df3)
+#df.shape
 
-data_lm = TextLMDataBunch.from_csv(path, 'ds0.csv', text_cols=0, label_cols=1)
-data_clas = TextClasDataBunch.from_csv(path, 'ds0.csv', vocab=data_lm.train_ds.vocab, bs=42, text_cols=0, label_cols=1)
+test = pd.read_csv(path + 'unknown_dsets/unknown_ds0.csv')
+#test_df.count
+#test_df1 = pd.read_csv(path + 'unknown_dsets/unknown_ds1.csv')
+#test_df2 = pd.read_csv(path + 'unknown_dsets/unknown_ds2.csv')
+#test_df2 = pd.read_csv(path + 'unknown_dsets/unknown_ds3.csv')
+#test
+
+data_lm = TextLMDataBunch.from_df(path,train_df=df,valid_df=df,test_df=test,text_cols=0,label_cols=1)
+#data_lm = TextLMDataBunch.from_csv(path, 'ds0.csv', text_cols=0, label_cols=1)
+data_clas = TextClasDataBunch.from_df(path, train_df=df,valid_df=df, vocab=data_lm.train_ds.vocab, bs=42, text_cols=0, label_cols=1)
+
 moms = (0.8,0.7)
-learn = language_model_learner(data_lm, AWD_LSTM)
-learn.unfreeze()
-learn.fit_one_cycle(4, slice(1e-2), moms=moms)
-learn.save_encoder('enc')
-learn = text_classifier_learner(data_clas, AWD_LSTM)
-learn.load_encoder('enc')
-learn.fit_one_cycle(4, moms=moms)
-learn.unfreeze()
-learn.fit_one_cycle(8, slice(1e-5,1e-3), moms=moms)
-test = "still wearing a cool smile. He turned it on the adder in his hand.“ That’s quite enough, Blackscale.”XXX “You- you speak parseltongue, Professor?” “I do. Something I picked up in my travels. It has its uses, as I’m sure you can attest.” Quirrel passed Blackscale back to her. Harry took him with numb hands, returning him to around her neck. Blackscale no longer hung limply; he was coiled now, attentive to the wizard in the room."
 
-learn.predict(test)
+learn = language_model_learner(data_lm, AWD_LSTM, drop_mult=0.5)
+learn.lr_find()
+learn.recorder.plot()
+
+learn.fit_one_cycle(4, slice(1e-01), moms=moms)
+
+learn.unfreeze()
+learn.fit_one_cycle(4, slice(1e-01), moms=moms)
+
+learn.save_encoder('enc')
+
+learn = text_classifier_learner(data_clas,AWD_LSTM,drop_mult=0.7)
+learn.lr_find()
+learn.recorder.plot()
+
+learn.load_encoder('enc')
+learn.fit_one_cycle(8,slice(1e-01), moms=moms)
+
+learn.unfreeze()
+learn.fit_one_cycle(8, slice(1e-01), moms=moms)
+
+preds = learn.get_preds(True)
+
+print(preds[0])
--- unknown_tocsv.py
+++ unknown_tocsv.py
@@ -4,18 +4,6 @@ import glob
 import os
 import json
 
-def read_files(path,label):
-    # Reads all text files located in the 'path' and assigns them to 'label' class
-    files = glob.glob(path+os.sep+label+os.sep+'*.txt')
-    texts=[]
-    for i,v in enumerate(files):
-        #if(i < 4):
-            print(i)
-            f=codecs.open(v,'r',encoding='utf-8')
-            texts.append((f.read(),label))
-            f.close()
-    return texts
-
 def main():
     collection = 'pan18' + os.sep + 'collection-info.json'
     problems = []
@@ -23,15 +11,15 @@ def main():
         for attrib in json.load(f):
             if(len(problems) < 4):
                 problems.append(attrib['problem-name'])
-    print(problems)
     for idx,problem in enumerate(problems):
-        
-    #f = open("pan18/problem00001/unknown/unknown00001.txt", "r")
-    #texts = []
-    #texts.append(f.read())
-    #with open('unknown_ds.csv', mode='w') as unknown:
-     #   unknown_writer = csv.writer(unknown, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-      #  unknown_writer.writerow(texts)
-
+        path_unknown = 'pan18' + os.sep + problem + os.sep + 'unknown'
+        files = os.listdir(path_unknown)
+        with open( 'unknown_dsets' + os.sep + 'unknown_ds'+ str(idx) + '.csv', mode='w') as unknown:
+            unknown_writer = csv.writer(unknown, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+            for name in files:
+                f = open(path_unknown + os.sep + name, "r")
+                texts = []
+                texts.append(f.read())
+                unknown_writer.writerow(texts)
 if __name__ == "__main__":
     main()
