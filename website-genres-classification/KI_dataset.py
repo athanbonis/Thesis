4402db62c7a0b49a06154de18e0e5142384be04b
Athan Bonis <athancb@hotmail.com>
2019-09-03 17:49:07 +0300

    KI-04 csv dataset ready

--- website-genres-classification/KI_dataset.py
+++ website-genres-classification/KI_dataset.py
@@ -2,11 +2,19 @@ import os
 import codecs
 import csv
 
-directory = os.path.join(os.environ["HOMEPATH"],"Desktop","KI-04")
+directory = os.path.join("C:/","GitHub","Thesis","website-genres-classification","KI-04")
 
 texts = []
-for f in os.listdir(directory):
-    f = codecs.open(f,'r',encoding="ISO-8859-1")
-    texts.append([f.split('_')[0],f.read()])
+for txtfile in os.listdir(directory):
+    f = codecs.open(os.path.join(directory,txtfile),'r',encoding="ISO-8859-1")
+    texts.append([txtfile.split('_')[0],f.read()])
     f.close()
 print(texts)
+
+with open('KI-04.csv',mode='w',encoding="ISO-8859-1") as ds:
+    dataset_writer = csv.writer(ds, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    dataset_writer.writerow(["label","text"])
+    for t in texts:
+        label = t[0]
+        text = t[1]
+        dataset_writer.writerow([label,text])
